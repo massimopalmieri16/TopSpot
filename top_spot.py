b0e7126a98a0fbc90138cd64aa5e42f12681531e
massimopalmieri16 <massimo.palmieri16@gmail.com>
2024-11-17 22:53:48 +0100

    Set page title

--- top_spot.py
+++ top_spot.py
@@ -68,6 +68,7 @@ def user_top_items(access_token):
 def main():
     st.title("TopSpot")
     st.subheader("Your Spotify top artists and tracks")
+    st.set_page_config(page_title="TopSpot")
     
     # Create OAuth2Component instance
     oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, TOKEN_URL, None)
